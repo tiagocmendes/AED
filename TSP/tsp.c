8c0ec0403f2c8a00a021da6f0acff73a1b5bfce3
Tiago Mendes <tiagocmendes@ua.pt>
2018-10-25 00:15:24 +0100

    Several updates

--- TSP/tsp.c
+++ TSP/tsp.c
@@ -8,6 +8,7 @@
 // solution of the traveling salesman problem
 //
 
+#include <assert.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -31,14 +32,43 @@ static long n_tours;
 // first solution (brute force, distance computed at the end, compute best and worst tours)
 //
 
+int computeTourLength(int n, int *a)
+{
+  int i, tourLength = 0;
+
+  for(i = 0;i < n-1;i++)
+  {
+    tourLength += cities[a[i]].distance[a[i+1]];
+  }
+  return tourLength += cities[a[0]].distance[a[n-1]];
+}
+
+void updateLengths(int n, int tourLength, int *a)
+{
+  int i;
+
+  if(tourLength < min_length)
+  { // update min_length and min_tour
+    min_length = tourLength;
+    for(i = 0;i < n;i++)
+     {
+      min_tour[i] = a[i];
+    }
+  }
+  else if(tourLength > max_length)
+  { // update max_length and max_tour
+    max_length = tourLength;
+    for(i = 0;i < n;i++)
+    {
+      max_tour[i] = a[i];
+    }
+  }
+}
+
 void tsp_v1(int n,int m,int *a)
 {
-
   
-  FILE *file;
-  int i,j,t;
-  int tourLength = 0;
-  file = fopen("test.csv", "a+");// a maneira como eu estruturei o codigo tive que fazer 'a+' em vez de 'w' para ir adicionando em cada linha a nova tour
+  int i,t,tourLength;
  
   if(m < n - 1)
     for(i = m;i < n;i++)
@@ -50,39 +80,32 @@ void tsp_v1(int n,int m,int *a)
       t = a[m];
       a[m] = a[i];
       a[i] = t;
-      
     }
   else
   { // visit permutation
     n_tours++;
-    for(j = 0;j < n-1;j++)
-    { // compute tourLength
-      tourLength += cities[a[j]].distance[a[j+1]];
-      
-    }
-    if(n==11){
-      fprintf(file,"%5d \n",tourLength);//se quiseres correr mais que n=11 eliminar codigo de escrever no file
-    }
-    if(tourLength < min_length)
-    { // update min_length and min_tour
-      min_length = tourLength;
-      for(j = 0;j < n;j++)
-      {
-        min_tour[j] = a[j];
-      }
-    }
-    else if(tourLength > max_length)
-    { // update max_length and max_tour
-      max_length = tourLength;
-      for(j = 0;j < n;j++)
-      {
-        max_tour[j] = a[j];
-      }
-    }
+    tourLength = computeTourLength(n,a);
+    updateLengths(n,tourLength,a);  // update min_length, min_tour, max_length and max_tour
   }
-  fclose(file);
 }
 
+void rand_perm(int n,int a[])
+{
+  int i,j,k;
+
+  for(i = 0;i < n;i++)
+  {
+    a[i] = i;
+  }
+  for(i = n - 1;i > 0;i--)
+  {
+    j = (int)floor((double)(i + 1) * (double)rand() / (1.0 + (double)RAND_MAX)); // range 0..i
+    assert(j >= 0 && j <= i);
+    k = a[i];
+    a[i] = a[j];
+    a[j] = k;
+  }
+}
 
 //
 // main program
@@ -90,7 +113,7 @@ void tsp_v1(int n,int m,int *a)
 
 int main(int argc,char **argv)
 {
-  int n_mec,special,n,i,a[max_n_cities];
+  int n_mec,special,n,i,j,a[max_n_cities];
   char file_name[32];
   double dt1;
 
@@ -99,10 +122,16 @@ int main(int argc,char **argv)
   init_cities_data(n_mec,special);
   printf("data for init_cities_data(%d,%d)\n",n_mec,special);
   fflush(stdout);
+
+  // open a file and initialize it
+  FILE *file;
+  file = fopen("tsp_data.csv", "w");
+  fprintf(file, "%s;%s;%s;%s;%s;%s\n","Number of cities (n)","Execution time (s)","minLength","minPath","maxLength","maxPath");
+
 #if 0
   print_distances();
 #endif
-  for(n = 3;n <= 11/*n_cities*/;n++)  // n_cities == 13 to reduce time execution
+  for(n = 3;n <= 14/*n_cities*/;n++)  // n_cities == 13 to reduce time execution
   {
     //
     // try tsp_v1
@@ -118,36 +147,61 @@ int main(int argc,char **argv)
       n_tours = 0l;
       tsp_v1(n,1,a); // no need to change the starting city, as we are making a tour
       dt1 = elapsed_time();
-      printf("tsp_v1() finished in %8.3fs (%ld tours generated)\n",dt1,n_tours);
+      printf("%d) tsp_v1() finished in %8.3fs (%ld tours generated)\n",n,dt1,n_tours);
+
+      // print min_tour by index
       printf("  min %5d [",min_length);
       for(i = 0;i < n;i++)
       {
         printf("%2d%s",min_tour[i],(i == n - 1) ? "] - " : ",");
       }
+
+      // print min_tour by city name
       for(i = 0;i < n;i++)
       {
         printf("%s%s", cities[min_tour[i]].name,(i == n - 1) ? "\n" : ",");
-      } 
+      }
+
+      // print max_tour by index
       printf("  max %5d [",max_length);
       for(i = 0;i < n;i++)
       {
         printf("%2d%s",max_tour[i],(i == n - 1) ? "] - " : ",");
       }
+
+      // print max_tour by city name
       for(i = 0;i < n;i++)
       {
-        printf("%s%s", cities[max_tour[i]].name,(i == n - 1) ? "\n" : ",");
-      } 
+        printf("%s%s", cities[max_tour[i]].name,(i == n - 1) ? "\n\n" : ",");
+      }
+
+      // save the computed data into a file
+      fprintf(file,"%d;%8.3f;%d;[",n,dt1,min_length);
+      for(i = 0; i < n;i++)
+      {
+        fprintf(file,"%d%s",min_tour[i],(i == n - 1) ? "];" : ",");
+      }
+      fprintf(file,"%d;[",max_length);
+      for(i = 0; i < n;i++)
+      {
+        fprintf(file,"%d%s",max_tour[i],(i == n - 1) ? "]\n" : ",");
+      }  
+
       fflush(stdout);
       if(argc == 2 && strcmp(argv[1],"-f") == 0)
       {
         min_tour[n] = -1;
-        sprintf(file_name,"min_%02d.svg",n);
+        sprintf(file_name,"./Maps/min_%02d.svg",n);
         make_map(file_name,min_tour);
         max_tour[n] = -1;
-        sprintf(file_name,"max_%02d.svg",n);
+        sprintf(file_name,"./Maps/max_%02d.svg",n);
         make_map(file_name,max_tour);
       }
+
+      // random permutations
+      
     }
   }
+  fclose(file);
   return 0;
 }
